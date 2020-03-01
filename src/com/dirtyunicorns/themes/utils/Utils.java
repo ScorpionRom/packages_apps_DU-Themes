shivatejapeddi/Themes

Scheduling: Added start and end times to schedule themes
- Get rid of the sunset to sunrise theme option.

Honestly, this would of been a kickass feature but in order to get an accurate
reading of sunset/sunrise, we would have to use GPS coordinates and then calcuate
it based on that and tbh I'm not about that life. So instead, we just give the user
the option to set a theme to be triggered at a specific start time and then set back
whatever theme they want afterwards.

- Fixed broadcast receiver not surviving a reboot
- Add scheduled theme time under summary
- Consolidate moar methods
- Fixed a few dozen or more NPE's and added some null checks just in case

Change-Id: I1709e885675738a043b2541855039e1625e0bebb

src/com/dirtyunicorns/themes/utils/Utils.java
@@ -17,21 +17,113 @@
 package com.dirtyunicorns.themes.utils;
 
 import static android.os.UserHandle.USER_SYSTEM;
+import static com.dirtyunicorns.themes.Themes.PREF_THEME_SCHEDULE;
+import static com.dirtyunicorns.themes.Themes.PREF_THEME_SCHEDULED_END_DATE;
+import static com.dirtyunicorns.themes.Themes.PREF_THEME_SCHEDULED_END_THEME;
+import static com.dirtyunicorns.themes.Themes.PREF_THEME_SCHEDULED_END_THEME_VALUE;
+import static com.dirtyunicorns.themes.Themes.PREF_THEME_SCHEDULED_END_TIME;
+import static com.dirtyunicorns.themes.Themes.PREF_THEME_SCHEDULED_START_DATE;
+import static com.dirtyunicorns.themes.Themes.PREF_THEME_SCHEDULED_START_THEME;
+import static com.dirtyunicorns.themes.Themes.PREF_THEME_SCHEDULED_START_THEME_VALUE;
+import static com.dirtyunicorns.themes.Themes.PREF_THEME_SCHEDULED_START_TIME;
 
 import android.app.Activity;
 import android.app.UiModeManager;
 import android.app.WallpaperInfo;
 import android.app.WallpaperManager;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.om.IOverlayManager;
 import android.os.RemoteException;
 import android.text.TextUtils;
+import android.widget.Button;
+
+import com.dirtyunicorns.themes.R;
 
 import com.android.internal.util.du.ThemesUtils;
 
 import java.util.Objects;
 
 public class Utils {
+
+    public static String getThemeSchedule(SharedPreferences mSharedPreferences) {
+        return mSharedPreferences.getString(PREF_THEME_SCHEDULE, "1");
+    }
+
+    public static String getScheduledStartTheme(SharedPreferences mSharedPreferences) {
+        return mSharedPreferences.getString(PREF_THEME_SCHEDULED_START_THEME, null);
+    }
+
+    public static String getScheduledStartThemeValue(SharedPreferences mSharedPreferences) {
+        return mSharedPreferences.getString(PREF_THEME_SCHEDULED_START_THEME_VALUE, null);
+    }
+
+    public static String getScheduledStartThemeTime(SharedPreferences mSharedPreferences) {
+        return mSharedPreferences.getString(PREF_THEME_SCHEDULED_START_TIME, null);
+    }
+
+    public static String getScheduledStartThemeDate(SharedPreferences mSharedPreferences) {
+        return mSharedPreferences.getString(PREF_THEME_SCHEDULED_START_DATE, null);
+    }
+
+    public static String getScheduledStartThemeSummary(SharedPreferences mSharedPreferences, Context context) {
+        String scheduledStartThemeSummary = mSharedPreferences.getString(PREF_THEME_SCHEDULED_START_THEME, null);
+
+        if (scheduledStartThemeSummary != null) {
+            switch (scheduledStartThemeSummary) {
+                case "1":
+                    scheduledStartThemeSummary = context.getString(R.string.theme_type_light);
+                    break;
+                case "2":
+                    scheduledStartThemeSummary = context.getString(R.string.theme_type_google_dark);
+                    break;
+                case "3":
+                    scheduledStartThemeSummary = context.getString(R.string.theme_type_pitch_black);
+                    break;
+                case "4":
+                    scheduledStartThemeSummary = context.getString(R.string.theme_type_solarized_dark);
+                    break;
+            }
+        }
+        return scheduledStartThemeSummary;
+    }
+
+    public static String getScheduledEndTheme(SharedPreferences mSharedPreferences) {
+        return mSharedPreferences.getString(PREF_THEME_SCHEDULED_END_THEME, null);
+    }
+
+    public static String getScheduledEndThemeValue(SharedPreferences mSharedPreferences) {
+        return mSharedPreferences.getString(PREF_THEME_SCHEDULED_END_THEME_VALUE, null);
+    }
+
+    public static String getScheduledEndThemeTime(SharedPreferences mSharedPreferences) {
+        return mSharedPreferences.getString(PREF_THEME_SCHEDULED_END_TIME, null);
+    }
+
+    public static String getScheduledEndThemeDate(SharedPreferences mSharedPreferences) {
+        return mSharedPreferences.getString(PREF_THEME_SCHEDULED_END_DATE, null);
+    }
+
+    public static String getScheduledEndThemeSummary(SharedPreferences mSharedPreferences, Context context) {
+        String scheduledEndThemeSummary = mSharedPreferences.getString(PREF_THEME_SCHEDULED_END_THEME, null);
+        if (scheduledEndThemeSummary != null) {
+            switch (scheduledEndThemeSummary) {
+                case "1":
+                    scheduledEndThemeSummary = context.getString(R.string.theme_type_light);
+                    break;
+                case "2":
+                    scheduledEndThemeSummary = context.getString(R.string.theme_type_google_dark);
+                    break;
+                case "3":
+                    scheduledEndThemeSummary = context.getString(R.string.theme_type_pitch_black);
+                    break;
+                case "4":
+                    scheduledEndThemeSummary = context.getString(R.string.theme_type_solarized_dark);
+                    break;
+            }
+        }
+        return scheduledEndThemeSummary;
+    }
 
     public static void handleOverlays(String packagename, Boolean state, IOverlayManager mOverlayManager) {
         try {
@@ -54,6 +146,15 @@ public class Utils {
             } catch (RemoteException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    public static void setForegroundDrawable(String packagename, Button buttonAccent, Activity activity) {
+        if (com.android.internal.util.du.Utils.isThemeEnabled(packagename)) {
+            buttonAccent.setForeground(activity.getResources().getDrawable(
+                    R.drawable.accent_picker_checkmark, null));
+        } else {
+            buttonAccent.setForeground(null);
         }
     }
 

ScorpionRom/packages_apps_DU-Themes

src/com/dirtyunicorns/themes/utils/Utils.java
@@ -17,21 +17,113 @@
 package com.dirtyunicorns.themes.utils;
 
 import static android.os.UserHandle.USER_SYSTEM;
+import static com.dirtyunicorns.themes.Themes.PREF_THEME_SCHEDULE;
+import static com.dirtyunicorns.themes.Themes.PREF_THEME_SCHEDULED_END_DATE;
+import static com.dirtyunicorns.themes.Themes.PREF_THEME_SCHEDULED_END_THEME;
+import static com.dirtyunicorns.themes.Themes.PREF_THEME_SCHEDULED_END_THEME_VALUE;
+import static com.dirtyunicorns.themes.Themes.PREF_THEME_SCHEDULED_END_TIME;
+import static com.dirtyunicorns.themes.Themes.PREF_THEME_SCHEDULED_START_DATE;
+import static com.dirtyunicorns.themes.Themes.PREF_THEME_SCHEDULED_START_THEME;
+import static com.dirtyunicorns.themes.Themes.PREF_THEME_SCHEDULED_START_THEME_VALUE;
+import static com.dirtyunicorns.themes.Themes.PREF_THEME_SCHEDULED_START_TIME;
 
 import android.app.Activity;
 import android.app.UiModeManager;
 import android.app.WallpaperInfo;
 import android.app.WallpaperManager;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.om.IOverlayManager;
 import android.os.RemoteException;
 import android.text.TextUtils;
+import android.widget.Button;
+
+import com.dirtyunicorns.themes.R;
 
 import com.android.internal.util.du.ThemesUtils;
 
 import java.util.Objects;
 
 public class Utils {
+
+    public static String getThemeSchedule(SharedPreferences mSharedPreferences) {
+        return mSharedPreferences.getString(PREF_THEME_SCHEDULE, "1");
+    }
+
+    public static String getScheduledStartTheme(SharedPreferences mSharedPreferences) {
+        return mSharedPreferences.getString(PREF_THEME_SCHEDULED_START_THEME, null);
+    }
+
+    public static String getScheduledStartThemeValue(SharedPreferences mSharedPreferences) {
+        return mSharedPreferences.getString(PREF_THEME_SCHEDULED_START_THEME_VALUE, null);
+    }
+
+    public static String getScheduledStartThemeTime(SharedPreferences mSharedPreferences) {
+        return mSharedPreferences.getString(PREF_THEME_SCHEDULED_START_TIME, null);
+    }
+
+    public static String getScheduledStartThemeDate(SharedPreferences mSharedPreferences) {
+        return mSharedPreferences.getString(PREF_THEME_SCHEDULED_START_DATE, null);
+    }
+
+    public static String getScheduledStartThemeSummary(SharedPreferences mSharedPreferences, Context context) {
+        String scheduledStartThemeSummary = mSharedPreferences.getString(PREF_THEME_SCHEDULED_START_THEME, null);
+
+        if (scheduledStartThemeSummary != null) {
+            switch (scheduledStartThemeSummary) {
+                case "1":
+                    scheduledStartThemeSummary = context.getString(R.string.theme_type_light);
+                    break;
+                case "2":
+                    scheduledStartThemeSummary = context.getString(R.string.theme_type_google_dark);
+                    break;
+                case "3":
+                    scheduledStartThemeSummary = context.getString(R.string.theme_type_pitch_black);
+                    break;
+                case "4":
+                    scheduledStartThemeSummary = context.getString(R.string.theme_type_solarized_dark);
+                    break;
+            }
+        }
+        return scheduledStartThemeSummary;
+    }
+
+    public static String getScheduledEndTheme(SharedPreferences mSharedPreferences) {
+        return mSharedPreferences.getString(PREF_THEME_SCHEDULED_END_THEME, null);
+    }
+
+    public static String getScheduledEndThemeValue(SharedPreferences mSharedPreferences) {
+        return mSharedPreferences.getString(PREF_THEME_SCHEDULED_END_THEME_VALUE, null);
+    }
+
+    public static String getScheduledEndThemeTime(SharedPreferences mSharedPreferences) {
+        return mSharedPreferences.getString(PREF_THEME_SCHEDULED_END_TIME, null);
+    }
+
+    public static String getScheduledEndThemeDate(SharedPreferences mSharedPreferences) {
+        return mSharedPreferences.getString(PREF_THEME_SCHEDULED_END_DATE, null);
+    }
+
+    public static String getScheduledEndThemeSummary(SharedPreferences mSharedPreferences, Context context) {
+        String scheduledEndThemeSummary = mSharedPreferences.getString(PREF_THEME_SCHEDULED_END_THEME, null);
+        if (scheduledEndThemeSummary != null) {
+            switch (scheduledEndThemeSummary) {
+                case "1":
+                    scheduledEndThemeSummary = context.getString(R.string.theme_type_light);
+                    break;
+                case "2":
+                    scheduledEndThemeSummary = context.getString(R.string.theme_type_google_dark);
+                    break;
+                case "3":
+                    scheduledEndThemeSummary = context.getString(R.string.theme_type_pitch_black);
+                    break;
+                case "4":
+                    scheduledEndThemeSummary = context.getString(R.string.theme_type_solarized_dark);
+                    break;
+            }
+        }
+        return scheduledEndThemeSummary;
+    }
 
     public static void handleOverlays(String packagename, Boolean state, IOverlayManager mOverlayManager) {
         try {
@@ -54,6 +146,15 @@ public class Utils {
             } catch (RemoteException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    public static void setForegroundDrawable(String packagename, Button buttonAccent, Activity activity) {
+        if (com.android.internal.util.custom.Utils.isThemeEnabled(packagename)) {
+            buttonAccent.setForeground(activity.getResources().getDrawable(
+                    R.drawable.accent_picker_checkmark, null));
+        } else {
+            buttonAccent.setForeground(null);
         }
     }
 