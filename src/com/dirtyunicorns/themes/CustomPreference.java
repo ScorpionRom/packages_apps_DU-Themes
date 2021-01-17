AOSiP/platform_packages_apps_DU-Themes

DU-Themes: Add status icons from p5
Change-Id: Ib4a3b8883235640728c072fae3f46302e22c0537

src/com/dirtyunicorns/themes/CustomPreference.java
@@ -52,6 +52,12 @@ public class CustomPreference extends Preference {
             setLayoutResource(R.layout.themes_main_rounded);
         } else if (AOSiPUtils.isThemeEnabled("com.android.theme.icon_pack.circular.android")) {
             setLayoutResource(R.layout.themes_main_circular);
+        } else if (AOSiPUtils.isThemeEnabled("com.android.theme.icon_pack.kai.android")) {
+            setLayoutResource(R.layout.themes_main_kai);
+        } else if (AOSiPUtils.isThemeEnabled("com.android.theme.icon_pack.sam.android")) {
+            setLayoutResource(R.layout.themes_main_sam);
+        } else if (AOSiPUtils.isThemeEnabled("com.android.theme.icon_pack.victor.android")) {
+            setLayoutResource(R.layout.themes_main_victor);
         } else {
             setLayoutResource(R.layout.themes_main);
         }

ScorpionRom/packages_apps_DU-Themes

src/com/dirtyunicorns/themes/CustomPreference.java
@@ -52,6 +52,12 @@ public class CustomPreference extends Preference {
             setLayoutResource(R.layout.themes_main_rounded);
         } else if (Utils.isThemeEnabled("com.android.theme.icon_pack.circular.android")) {
             setLayoutResource(R.layout.themes_main_circular);
+        } else if (Utils.isThemeEnabled("com.android.theme.icon_pack.kai.android")) {
+            setLayoutResource(R.layout.themes_main_kai);
+        } else if (Utils.isThemeEnabled("com.android.theme.icon_pack.sam.android")) {
+            setLayoutResource(R.layout.themes_main_sam);
+        } else if (Utils.isThemeEnabled("com.android.theme.icon_pack.victor.android")) {
+            setLayoutResource(R.layout.themes_main_victor);
         } else {
             setLayoutResource(R.layout.themes_main);
         }